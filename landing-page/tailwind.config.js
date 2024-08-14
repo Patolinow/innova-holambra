80bf5088daa35e3f6bf12cb3de4a39b1eddef454
Patolinow <fabiodasn1@gmail.com>
2024-08-14 20:38:54 -0300

    feat: added custom properties to tailwind.config

--- landing-page/tailwind.config.js
+++ landing-page/tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('assets/images/background.svg')",
+      },
+      maxWidth: {
+        'view': '1200px'
+      },
+      fontSize: {
+        '4.5xl': '2.5rem'
+      },
+      fontFamily: {
+        'gibson': ['Gibson', 'sans-serif']
+      },
+      colors: {
+        'light-blue': '#4CBDB1',
+        'dark-blue': '#003262',
+        'black': '#212529',
+      }
+    },
   },
   plugins: [],
 }
